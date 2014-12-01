8ceccfed110ad2006e97f239dbdc47c43c79a48f
Mateo Inchaurrandieta <mateo.inchaurrandieta.13@ucl.ac.uk>
2014-12-01 18:35:06 +0000

    Added AdminUser stub class. I'll add functionality once DBAdapter is coded

---
+++ user/AdminUser.java
@@ -0,0 +1,22 @@
+package user;
+
+import java.sql.Date;
+
+public class AdminUser extends User {
+
+	public AdminUser(String username, String hashedPassword, boolean isAdmin,
+			DBAdapter database) {
+		super(username, hashedPassword, true, database);
+	}
+
+	@Override
+	public boolean requestBooking(User user, Room room, Date date) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public boolean forceBooking(User user, Room room, Date date) {
+		return false;
+	}
+
+}
