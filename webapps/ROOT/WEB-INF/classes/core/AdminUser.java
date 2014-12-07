9e3826c5d36b71a0855983f573c303c56f6ee954
Mateo Inchaurrandieta <mateo.inchaurrandieta.13@ucl.ac.uk>
2014-12-07 16:39:57 +0000

    Added requestBooking and forceBooking to Admins

--- webapps/ROOT/WEB-INF/classes/core/AdminUser.java
+++ webapps/ROOT/WEB-INF/classes/core/AdminUser.java
@@ -1,6 +1,10 @@
 package core;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import utils.DBAdapter;
 
@@ -12,13 +16,52 @@ public class AdminUser extends User {
 	}
 
 	@Override
-	public boolean requestBooking(User user, Room room, Date start, Date end) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean requestBooking(User user, Room room, Date start, Date end) throws SQLException {
+		Connection conn;
+		try {
+			conn = DBAdapter.dbConnect();
+		} catch (ClassNotFoundException e1) {
+			// connection failed
+			return false;
+		}
+
+		try {
+			if (user.hasBooking(start)) {
+				return false;
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			return false;
+		}
+
+		String newBooking = "INSERT INTO bookings (room_id, username, start, end) VALUES(?, ?, ?, ?);";
+		List<Object> args = new ArrayList<Object>();
+		args.add(user.getUsername());
+		args.add(room.getName());
+		args.add(start);
+		args.add(end);
+
+		DBAdapter.dbExecute(newBooking, args, true, conn);
+		return true;
 	}
-	
-	public boolean forceBooking(User user, Room room, Date start, Date end) {
-		return false;
+
+	public boolean forceBooking(User user, Room room, Date start, Date end) throws SQLException {
+		Connection conn;
+		try {
+			conn = DBAdapter.dbConnect();
+		} catch (ClassNotFoundException e1) {
+			// connection failed
+			return false;
+		}
+
+		String newBooking = "INSERT INTO bookings (room_id, username, start, end) VALUES(?, ?, ?, ?);";
+		List<Object> args = new ArrayList<Object>();
+		args.add(getUsername());
+		args.add(room.getName());
+		args.add(start);
+		args.add(end);
+
+		DBAdapter.dbExecute(newBooking, args, true, conn);
+		return true;
 	}
 
 }
