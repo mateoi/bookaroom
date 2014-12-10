db93a33232c510b1ad4c83df4dc06f91c3414ced
Andrew Ghobrial <awghobrial@gmail.com>
2014-12-10 15:00:06 +0000

    lkj

--- webapps/ROOT/WEB-INF/classes/utils/LoginSystem.java
+++ webapps/ROOT/WEB-INF/classes/utils/LoginSystem.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Created by andrewwg94 on 12/5/14.
@@ -37,5 +41,30 @@ public class LoginSystem {
             for(Cookie c: cookies) if(c.getName().equals("session_id")) session_id = c.getValue();
         }
         return DigestUtils.md5Hex(authVal).equals(session_id);
+    }
+    public static boolean logIn(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        boolean success = false;
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        ArrayList<Object> list = new ArrayList<Object>();
+        list.add(username);
+        if(username!=null && password !=null && !username.equals("") && !password.equals("")) {
+            Connection cxn = null;
+            try {
+                cxn = DBAdapter.dbConnect();
+                ResultSet rs = DBAdapter.dbExecute("SELECT username, password FROM users WHERE username=?", list, false, cxn);
+                if(rs.next()) {
+                    success = password.equals(rs.getString("password"));
+                    if (success) {
+                        saveSession(session, request, response, username);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if(cxn!=null) try{cxn.close();} catch(SQLException e) {e.printStackTrace();}
+            }
+        }
+    return success;
     }
 }
---
+++ webapps/ROOT/index_2.jsp
@@ -0,0 +1,30 @@
+<%@ page import="org.apache.commons.codec.digest.DigestUtils" %>
+<%@ page import="utils.LoginSystem" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Book A Room: Homepage</title>
+</head>
+<body>
+     <h1>Home Page</h1>
+      <pre>
+       The purpose of this page:
+          - check if user is logged in. if the user is logged in, display the name, if not display login button
+          - link to the search, booking, and available now pages
+          - more button to show about, contact, and help info
+----------------------------------------------------------------------------------------------------------------
+      </pre>
+
+<a href="RoomBrowse.jsp">Browse</a><br>
+<a href="MyBookings.jsp">My Bookings</a><br>
+<%
+    if(LoginSystem.isAuthenticated(session, request)) {
+        out.println("LOGEED IN AS: " + session.getAttribute("username"));
+        out.println("<a href=\"logout.jsp\">Logout</a>");
+    }
+    else out.println("<a href=\"login.jsp\">Please Login</a>");
+%>
+</body>
+</html>
